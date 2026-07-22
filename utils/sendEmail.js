78131286e145538b1f69aacbeb4af61f7bba6975
Lotachukwu <lotachukwu134@gmail.com>
2026-07-22 01:58:54 +0100

    port

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -173,8 +173,8 @@ const createTransporter = () => {
 
   return nodemailer.createTransport({
     host:   process.env.EMAIL_HOST,
-    port:   Number(process.env.EMAIL_PORT) || 465,
-    secure: true,
+    port:   Number(process.env.EMAIL_PORT) || 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
